17227b457cc6729be17c43673571d05be13580c5
Oluwaseun Otubanjo <63556860+omo1933@users.noreply.github.com>
2022-01-28 07:48:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,172 @@
 class CardDeck {
-	constructor(deckElement, handElement) {
-		this.deckElement = document.querySelector(deckElement);
-		this.handElement = document.querySelector(handElement);
-		this.deck = [
-			{ id: "c-2", name: "2 of Clubs", rank: 2, suit: "clubs" },
-			{ id: "c-3", name: "3 of Clubs", rank: 3, suit: "clubs" },
-			{ id: "c-4", name: "4 of Clubs", rank: 4, suit: "clubs" },
-			{ id: "c-5", name: "5 of Clubs", rank: 5, suit: "clubs" },
-			{ id: "c-6", name: "6 of Clubs", rank: 6, suit: "clubs" },
-			{ id: "c-7", name: "7 of Clubs", rank: 7, suit: "clubs" },
-			{ id: "c-8", name: "8 of Clubs", rank: 8, suit: "clubs" },
-			{ id: "c-9", name: "9 of Clubs", rank: 9, suit: "clubs" },
-			{ id: "c-10", name: "10 of Clubs", rank: 10, suit: "clubs" },
-			{ id: "c-J", name: "Jack of Clubs", rank: 11, suit: "clubs" },
-			{ id: "c-Q", name: "Queen of Clubs", rank: 12, suit: "clubs" },
-			{ id: "c-K", name: "King of Clubs", rank: 13, suit: "clubs" },
-			{ id: "c-A", name: "Ace of Clubs", rank: 14, suit: "clubs" },
-			{ id: "d-2", name: "2 of Diamonds", rank: 2, suit: "diamonds" },
-			{ id: "d-3", name: "3 of Diamonds", rank: 3, suit: "diamonds" },
-			{ id: "d-4", name: "4 of Diamonds", rank: 4, suit: "diamonds" },
-			{ id: "d-5", name: "5 of Diamonds", rank: 5, suit: "diamonds" },
-			{ id: "d-6", name: "6 of Diamonds", rank: 6, suit: "diamonds" },
-			{ id: "d-7", name: "7 of Diamonds", rank: 7, suit: "diamonds" },
-			{ id: "d-8", name: "8 of Diamonds", rank: 8, suit: "diamonds" },
-			{ id: "d-9", name: "9 of Diamonds", rank: 9, suit: "diamonds" },
-			{ id: "d-10", name: "10 of Diamonds", rank: 10, suit: "diamonds" },
-			{ id: "d-J", name: "Jack of Diamonds", rank: 11, suit: "diamonds" },
-			{ id: "d-Q", name: "Queen of Diamonds", rank: 12, suit: "diamonds" },
-			{ id: "d-K", name: "King of Diamonds", rank: 13, suit: "diamonds" },
-			{ id: "d-A", name: "Ace of Diamonds", rank: 14, suit: "diamonds" },
-			{ id: "h-2", name: "2 of Hearts", rank: 2, suit: "hearts" },
-			{ id: "h-3", name: "3 of Hearts", rank: 3, suit: "hearts" },
-			{ id: "h-4", name: "4 of Hearts", rank: 4, suit: "hearts" },
-			{ id: "h-5", name: "5 of Hearts", rank: 5, suit: "hearts" },
-			{ id: "h-6", name: "6 of Hearts", rank: 6, suit: "hearts" },
-			{ id: "h-7", name: "7 of Hearts", rank: 7, suit: "hearts" },
-			{ id: "h-8", name: "8 of Hearts", rank: 8, suit: "hearts" },
-			{ id: "h-9", name: "9 of Hearts", rank: 9, suit: "hearts" },
-			{ id: "h-10", name: "10 of Hearts", rank: 10, suit: "hearts" },
-			{ id: "h-J", name: "Jack of Hearts", rank: 11, suit: "hearts" },
-			{ id: "h-Q", name: "Queen of Hearts", rank: 12, suit: "hearts" },
-			{ id: "h-K", name: "King of Hearts", rank: 13, suit: "hearts" },
-			{ id: "h-A", name: "Ace of Hearts", rank: 14, suit: "hearts" },
-			{ id: "s-2", name: "2 of Spades", rank: 2, suit: "spades" },
-			{ id: "s-3", name: "3 of Spades", rank: 3, suit: "spades" },
-			{ id: "s-4", name: "4 of Spades", rank: 4, suit: "spades" },
-			{ id: "s-5", name: "5 of Spades", rank: 5, suit: "spades" },
-			{ id: "s-6", name: "6 of Spades", rank: 6, suit: "spades" },
-			{ id: "s-7", name: "7 of Spades", rank: 7, suit: "spades" },
-			{ id: "s-8", name: "8 of Spades", rank: 8, suit: "spades" },
-			{ id: "s-9", name: "9 of Spades", rank: 9, suit: "spades" },
-			{ id: "s-10", name: "10 of Spades", rank: 10, suit: "spades" },
-			{ id: "s-J", name: "Jack of Spades", rank: 11, suit: "spades" },
-			{ id: "s-Q", name: "Queen of Spades", rank: 12, suit: "spades" },
-			{ id: "s-K", name: "King of Spades", rank: 13, suit: "spades" },
-			{ id: "s-A", name: "Ace of Spades", rank: 14, suit: "spades" },
-		];
-		this.possibleCards = [...this.deck];
-		this.hand = [];
-		this.init();
-	}
-	init() {
-		this.shuffleDeck();
+  constructor(deckElement, handElement) {
+    this.deckElement = document.querySelector(deckElement);
+    this.handElement = document.querySelector(handElement);
+    this.deck = [
+      { id: "c-2", name: "2 of Clubs", rank: 2, suit: "clubs" },
+      { id: "c-3", name: "3 of Clubs", rank: 3, suit: "clubs" },
+      { id: "c-4", name: "4 of Clubs", rank: 4, suit: "clubs" },
+      { id: "c-5", name: "5 of Clubs", rank: 5, suit: "clubs" },
+      { id: "c-6", name: "6 of Clubs", rank: 6, suit: "clubs" },
+      { id: "c-7", name: "7 of Clubs", rank: 7, suit: "clubs" },
+      { id: "c-8", name: "8 of Clubs", rank: 8, suit: "clubs" },
+      { id: "c-9", name: "9 of Clubs", rank: 9, suit: "clubs" },
+      { id: "c-10", name: "10 of Clubs", rank: 10, suit: "clubs" },
+      { id: "c-J", name: "Jack of Clubs", rank: 11, suit: "clubs" },
+      { id: "c-Q", name: "Queen of Clubs", rank: 12, suit: "clubs" },
+      { id: "c-K", name: "King of Clubs", rank: 13, suit: "clubs" },
+      { id: "c-A", name: "Ace of Clubs", rank: 14, suit: "clubs" },
+      { id: "d-2", name: "2 of Diamonds", rank: 2, suit: "diamonds" },
+      { id: "d-3", name: "3 of Diamonds", rank: 3, suit: "diamonds" },
+      { id: "d-4", name: "4 of Diamonds", rank: 4, suit: "diamonds" },
+      { id: "d-5", name: "5 of Diamonds", rank: 5, suit: "diamonds" },
+      { id: "d-6", name: "6 of Diamonds", rank: 6, suit: "diamonds" },
+      { id: "d-7", name: "7 of Diamonds", rank: 7, suit: "diamonds" },
+      { id: "d-8", name: "8 of Diamonds", rank: 8, suit: "diamonds" },
+      { id: "d-9", name: "9 of Diamonds", rank: 9, suit: "diamonds" },
+      { id: "d-10", name: "10 of Diamonds", rank: 10, suit: "diamonds" },
+      { id: "d-J", name: "Jack of Diamonds", rank: 11, suit: "diamonds" },
+      { id: "d-Q", name: "Queen of Diamonds", rank: 12, suit: "diamonds" },
+      { id: "d-K", name: "King of Diamonds", rank: 13, suit: "diamonds" },
+      { id: "d-A", name: "Ace of Diamonds", rank: 14, suit: "diamonds" },
+      { id: "h-2", name: "2 of Hearts", rank: 2, suit: "hearts" },
+      { id: "h-3", name: "3 of Hearts", rank: 3, suit: "hearts" },
+      { id: "h-4", name: "4 of Hearts", rank: 4, suit: "hearts" },
+      { id: "h-5", name: "5 of Hearts", rank: 5, suit: "hearts" },
+      { id: "h-6", name: "6 of Hearts", rank: 6, suit: "hearts" },
+      { id: "h-7", name: "7 of Hearts", rank: 7, suit: "hearts" },
+      { id: "h-8", name: "8 of Hearts", rank: 8, suit: "hearts" },
+      { id: "h-9", name: "9 of Hearts", rank: 9, suit: "hearts" },
+      { id: "h-10", name: "10 of Hearts", rank: 10, suit: "hearts" },
+      { id: "h-J", name: "Jack of Hearts", rank: 11, suit: "hearts" },
+      { id: "h-Q", name: "Queen of Hearts", rank: 12, suit: "hearts" },
+      { id: "h-K", name: "King of Hearts", rank: 13, suit: "hearts" },
+      { id: "h-A", name: "Ace of Hearts", rank: 14, suit: "hearts" },
+      { id: "s-2", name: "2 of Spades", rank: 2, suit: "spades" },
+      { id: "s-3", name: "3 of Spades", rank: 3, suit: "spades" },
+      { id: "s-4", name: "4 of Spades", rank: 4, suit: "spades" },
+      { id: "s-5", name: "5 of Spades", rank: 5, suit: "spades" },
+      { id: "s-6", name: "6 of Spades", rank: 6, suit: "spades" },
+      { id: "s-7", name: "7 of Spades", rank: 7, suit: "spades" },
+      { id: "s-8", name: "8 of Spades", rank: 8, suit: "spades" },
+      { id: "s-9", name: "9 of Spades", rank: 9, suit: "spades" },
+      { id: "s-10", name: "10 of Spades", rank: 10, suit: "spades" },
+      { id: "s-J", name: "Jack of Spades", rank: 11, suit: "spades" },
+      { id: "s-Q", name: "Queen of Spades", rank: 12, suit: "spades" },
+      { id: "s-K", name: "King of Spades", rank: 13, suit: "spades" },
+      { id: "s-A", name: "Ace of Spades", rank: 14, suit: "spades" },
+    ];
+    this.possibleCards = [...this.deck];
+    this.hand = [];
+    this.init();
+  }
+  init() {
+    this.shuffleDeck();
 
-		let cardElements = document.querySelectorAll(".card");
+    let cardElements = document.querySelectorAll(".card");
 
-		cardElements.forEach((cardElement) => {
-			cardElement.addEventListener("click", (e) => {
-				if (cardElement.dataset.inHand == "true") {
-					this.discard(cardElement.id);
-				} else {
-					this.draw(cardElement.id);
-				}
-			});
-		});
-	}
+    cardElements.forEach((cardElement) => {
+      cardElement.addEventListener("click", (e) => {
+        if (cardElement.dataset.inHand == "true") {
+          this.discard(cardElement.id);
+        } else {
+          this.draw(cardElement.id);
+        }
+      });
+    });
+  }
 
-	generateRandomNumber(min, max) {
-		return Math.random() * (max - min) + min;
-	}
+  generateRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-	buildCard(card, inHand = false) {
-		return `
+  buildCard(card, inHand = false) {
+    return `
 		<div id="${card.id}" class="card" data-in-hand="${inHand}" data-rank="${card.rank}" data-suit="${card.suit}">
 			<div class="card-face-wrapper">
 				<img alt="${card.name}" src="assets/cards/${card.id}.svg">
 				<div class="card-back"></div>
 			</div>
 		</div>`;
-	}
+  }
 
-	buildDeck() {
-		this.deckElement.innerHTML = "";
+  buildDeck() {
+    this.deckElement.innerHTML = "";
 
-		this.deck.forEach((card) => {
-			let cardString = this.buildCard(card);
-			this.deckElement.insertAdjacentHTML("beforeend", cardString);
-		});
-		let cardElements = this.deckElement.children;
-		for (let cardElement of cardElements) {
-			this.ruffleCardElement(cardElement);
-		}
-	}
+    this.deck.forEach((card) => {
+      let cardString = this.buildCard(card);
+      this.deckElement.insertAdjacentHTML("beforeend", cardString);
+    });
+    let cardElements = this.deckElement.children;
+    for (let cardElement of cardElements) {
+      this.ruffleCardElement(cardElement);
+    }
+  }
 
-	shuffleDeck() {
-		for (let i = this.deck.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-		}
-		this.buildDeck();
-		this.possibleCards = [...this.deck];
-	}
+  shuffleDeck() {
+    for (let i = this.deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+    }
+    this.buildDeck();
+    this.possibleCards = [...this.deck];
+  }
 
-	ruffleCardElement(cardElement) {
-		let rotation = this.generateRandomNumber(-10, 10) + "deg";
-		let X = this.generateRandomNumber(-10, 10) + "px";
-		let Y = this.generateRandomNumber(-10, 10) + "px";
-		cardElement.style.transform = `rotate(${rotation}) translate3D(${X}, ${Y}, 0px)`;
-	}
-	draw(id) {
-		let card = this.deck.find((x) => x.id === id);
-		let cardElement = document.getElementById(id);
-		cardElement.style.transform = "";
+  ruffleCardElement(cardElement) {
+    let rotation = this.generateRandomNumber(-10, 10) + "deg";
+    let X = this.generateRandomNumber(-10, 10) + "px";
+    let Y = this.generateRandomNumber(-10, 10) + "px";
+    cardElement.style.transform = `rotate(${rotation}) translate3D(${X}, ${Y}, 0px)`;
+  }
+  draw(id) {
+    let card = this.deck.find((x) => x.id === id);
+    let cardElement = document.getElementById(id);
+    cardElement.style.transform = "";
 
-		this.hand.push(card);
-		this.deck = this.deck.filter((deckCard) => deckCard.id != id);
+    this.hand.push(card);
+    this.deck = this.deck.filter((deckCard) => deckCard.id != id);
 
-		this.handElement.appendChild(cardElement);
-		setTimeout(() => {
-			document.getElementById(id).dataset.inHand = "true";
-		}, 10);
-	}
+    this.handElement.appendChild(cardElement);
+    setTimeout(() => {
+      document.getElementById(id).dataset.inHand = "true";
+    }, 10);
+  }
 
-	discard(id) {
-		let card = this.hand.find((x) => x.id === id);
-		let cardElement = document.getElementById(id);
+  discard(id) {
+    let card = this.hand.find((x) => x.id === id);
+    let cardElement = document.getElementById(id);
 
-		this.deck.push(card);
-		this.hand = this.hand.filter((handCard) => handCard.id != id);
+    this.deck.push(card);
+    this.hand = this.hand.filter((handCard) => handCard.id != id);
 
-		document.getElementById(id).dataset.inHand = "false";
-		setTimeout(() => {
-			this.deckElement.appendChild(cardElement);
-			this.ruffleCardElement(cardElement);
-		}, 600);
-	}
+    document.getElementById(id).dataset.inHand = "false";
+    setTimeout(() => {
+      this.deckElement.appendChild(cardElement);
+      this.ruffleCardElement(cardElement);
+    }, 600);
+  }
 
-	sort() {
-		this.possibleCards = this.possibleCards.sort((a, b) =>
-			a.suit > b.suit ? 1 : a.suit === b.suit ? (a.rank > b.rank ? 1 : -1) : -1
-		);
-	}
+  sort() {
+    this.possibleCards = this.possibleCards.sort((a, b) =>
+      a.suit > b.suit ? 1 : a.suit === b.suit ? (a.rank > b.rank ? 1 : -1) : -1
+    );
+  }
 
-	filter(cardProp = null, values = []) {
-		this.possibleCards = this.possibleCards.filter((card) => values.includes(card[cardProp]));
-	}
+  filter(cardProp = null, values = []) {
+    this.possibleCards = this.possibleCards.filter((card) =>
+      values.includes(card[cardProp])
+    );
+  }
 
-	limit(number) {
-		this.possibleCards = this.possibleCards.slice(0, number);
-	}
+  limit(number) {
+    this.possibleCards = this.possibleCards.slice(0, number);
+  }
 
-	drawFiltered() {
-		this.possibleCards.forEach((card) => {
-			this.draw(card.id);
-		});
-	}
+  drawFiltered() {
+    this.possibleCards.forEach((card) => {
+      this.draw(card.id);
+    });
+  }
 }
 
 /*------------------------------------------*/
@@ -175,52 +177,41 @@ class CardDeck {
 /*------------------------------------------*/
 // Create a new card deck.
 const deck = new CardDeck(".deck", ".hand");
-let url= window.location.href;
+let url = window.location.href;
 
 //cards
- if(url.includes('index.html?cards=s-3+c-9+h-A')){
-	deck.filter('id',['s-3', 'c-9', 'h-A']);
-	deck.drawFiltered()
-}
-
-else if(url.includes('index.html?cards=s-3')){
-	deck.filter('id',['s-3']);
-	deck.drawFiltered()
-}
+const cards = (cards, urlParameter) => {
+  if (url.includes(`index.html?cards=${urlParameter}`)) {
+    deck.filter("id", [cards]);
+    deck.drawFiltered();
+  }
+};
 
 //suits
-else if(url.includes('index.html?suits=spades+hearts')){
-	deck.filter('suit',['spades', 'hearts']);
-	deck.drawFiltered();
-}
-else if(url.includes('index.html?suits=spades')){
-	deck.filter('suit',['spades']);
-	deck.drawFiltered();
-}
+const suits = (suits, urlParameter) => {
+  if (url.includes(`index.html?suits=${urlParameter}`)) {
+    deck.filter("id", [suits]);
+    deck.drawFiltered();
+  }
+};
 
 //ranks
-else if(url.includes('index.html?ranks=3+5+13')){
-	deck.filter('rank',[3,5,13]);
-	deck.drawFiltered();
-}
-else if(url.includes('index.html?ranks=3')){
-	deck.filter('rank',[3]);
-	deck.drawFiltered();
-}
+const ranks = (ranks, urlParameter) => {
+  if (url.includes(`index.html?ranks=${urlParameter}`)) {
+    deck.filter("id", [ranks]);
+    deck.drawFiltered();
+  }
+};
 
 //limit
-else if(url.includes('index.html?suits=clubs+hearts&ranks=2+6+10&limit=4')){
-	deck.filter('suit',['clubs', 'hearts']);
-	deck.filter('rank',[2,6,10])
-	deck.limit(4);
-	deck.drawFiltered();
-}
-
-else if(url.includes('index.html?suits=spades&limit=4')){
-	deck.filter('suit',['spades']);
-	deck.limit(4);
-	deck.drawFiltered();
-}
+const limit = (suits, ranks, limit, urlParameter) => {
+  if (url.includes(`index.html?suits=${urlParameter}`)) {
+    deck.filter("suit", [suits]);
+    deck.filter("rank", [ranks]);
+    deck.limit(limit);
+    deck.drawFiltered();
+  }
+};
 
 // Take a look at the deck object and its methods.
 console.log(deck);
